e82f835c6a19dfe52e443db15db90e19e8dab8dd
DeepakAvadhani <87537653+DeepakAvadhani@users.noreply.github.com>
2024-07-31 21:09:06 +0530

    updated some files

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,12 +1,10 @@
 const { Sequelize } = require('sequelize');
 
-// Create a new instance of Sequelize
 const sequelize = new Sequelize('crud_api', 'root', '', {
   host: '127.0.0.1',
   dialect: 'mysql'
 });
 
-// Test the connection
 sequelize.authenticate()
   .then(() => {
     console.log('Connection to the database has been established successfully.');
