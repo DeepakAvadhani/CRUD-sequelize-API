fd19bdf6791dcf334a0b67bb34087eacf51fed69
Deepak Avadhani <87537653+DeepakAvadhani@users.noreply.github.com>
2025-01-21 16:28:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use("/api/products", productRoute);
-dddd
+
 app.get("/", (req, res) => {
   res.send("Server is Up and running!");
 });
